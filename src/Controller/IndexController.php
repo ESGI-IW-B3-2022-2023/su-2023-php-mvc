395887679852f467a6e04a0c7a3f2e14c9be9cd8
Arnaud Gouel <agouel@micropole.com>
2023-07-07 15:32:44 +0200

    fix : remove merge conflict syntax

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -10,15 +10,6 @@ class IndexController extends AbstractController
   #[Route("/", name: "homepage")]
   public function home(): string
   {
-<<<<<<< Updated upstream
-    var_dump($this->em->getRepository(User::class)->findAll());
-=======
-    // $user = new User();
-    // $user->setName('Jean');
-    // $this->em->persist($user);
-    // $this->em->flush();
-    // var_dump($this->em->getRepository(User::class)->findAll());
->>>>>>> Stashed changes
     return $this->twig->render('index.html.twig');
   }
 }
