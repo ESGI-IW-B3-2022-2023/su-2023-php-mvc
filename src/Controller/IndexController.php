866b5a199345a6d8cce5ea2a9fac674cbc4c5264
Antoine <falgitoine@gmail.com>
2023-07-07 14:19:19 +0200

    Amélioration des users grace à une boucle dans UserController.php

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -10,10 +10,6 @@ class IndexController extends AbstractController
   #[Route("/", name: "homepage")]
   public function home(): string
   {
-    $user = new User();
-    $user->setName('Jean');
-    $this->em->persist($user);
-    $this->em->flush();
     var_dump($this->em->getRepository(User::class)->findAll());
     return $this->twig->render('index.html.twig');
   }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,16 +3,22 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManager;
 
 class UserController extends AbstractController
 {
+  #[Route("/user/create", name: "add_user")]
   public function create(EntityManager $em): string
   {
-    $user = new User();
-    $user->setName('Ora Morton');
-    $em->persist($user);
-    $em->flush();
+    $userNames = ['Lisa', 'Kevin', 'Emma', 'David', 'Louis', 'Etienne', 'Jerome', 'Louise', 'Didier'];
+
+    foreach ($userNames as $name) {
+        $user = new User();
+        $user->setName($name);
+        $em->persist($user);
+    }
+    $em->flush();    
 
     return $this->twig->render(
       'user/create_confirm.html.twig',
